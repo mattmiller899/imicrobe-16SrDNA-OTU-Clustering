5cdd643acf0d4c57e954434c0aafb8be2ddc01a5
Joshua Lynch <jklynch@email.arizona.edu>
2017-06-14 17:24:22 +0100

    add minimum overlap argument

--- stampede/template.sh
+++ stampede/template.sh
@@ -2,7 +2,7 @@
 
 echo "Started $(date)"
 
-sh run.sh ${INPUT_DIR} ${FORWARD_PRIMER} ${REVERSE_PRIMER}
+sh run.sh ${INPUT_DIR} ${FORWARD_PRIMER} ${REVERSE_PRIMER} ${MIN_OVERLAP}
 
 echo "Ended $(date)"
 exit 0
