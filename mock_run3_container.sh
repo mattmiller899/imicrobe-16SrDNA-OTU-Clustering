4392a0f466a48006f89a8c17b534542e7f54e614
Joshua Lynch <jklynch@email.arizona.edu>
2017-06-28 18:49:40 +0100

    add new command line arguments

--- mock_run3_container.sh
+++ mock_run3_container.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-rm -rf mock_run3/output
-mkdir mock_run3/output
+rm -rf mock_run3/container_output
+mkdir mock_run3/container_output
 
 time singularity run singularity/imicrobe-16SrDNA-OTU-Clustering.img \
   --input-dir ~/host/project/imicrobe/apps/imicrobe-16SrDNA-OTU-Clustering/mock_run3/input \
-  --output-dir ~/host/project/imicrobe/apps/imicrobe-16SrDNA-OTU-Clustering/mock_run3/output \
-  --core-count 1
+  --work-dir ~/host/project/imicrobe/apps/imicrobe-16SrDNA-OTU-Clustering/mock_run3/container_output \
+  --core-count 1 \
+  --cutadapt-min-length 100 \
+  --pear-min-overlap 200\
+  --pear-max-assembly-length 270 \
+  --pear-min-assembly-length 220 \
+  --vsearch-filter-maxee 1 \
+  --vsearch-filter-trunclen 245
--- mock_run3_pipeline.sh
+++ mock_run3_pipeline.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-rm -rf mock_run3/output
-mkdir -p mock_run3/output
+rm -rf mock_run3/pipeline_output
+mkdir -p mock_run3/pipeline_output
 
 time pipeline \
   --input-dir ~/host/project/imicrobe/apps/imicrobe-16SrDNA-OTU-Clustering/mock_run3/input \
-  --output-dir ~/host/project/imicrobe/apps/imicrobe-16SrDNA-OTU-Clustering/mock_run3/output \
-  --core-count 1
+  --work-dir ~/host/project/imicrobe/apps/imicrobe-16SrDNA-OTU-Clustering/mock_run3/pipeline_output \
+  --core-count 1 \
+  --cutadapt-min-length 100 \
+  --pear-min-overlap 200\
+  --pear-max-assembly-length 270 \
+  --pear-min-assembly-length 220 \
+  --vsearch-filter-maxee 1 \
+  --vsearch-filter-trunclen 245
