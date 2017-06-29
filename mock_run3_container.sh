a5f00cfc8b475ed170900ea21c71af2c8a0243f3
Joshua Lynch <jklynch@email.arizona.edu>
2017-06-29 14:20:14 +0100

    add argument for step 06

--- mock_run3_container.sh
+++ mock_run3_container.sh
@@ -6,10 +6,11 @@ mkdir mock_run3/container_output
 time singularity run singularity/imicrobe-16SrDNA-OTU-Clustering.img \
   --input-dir ~/host/project/imicrobe/apps/imicrobe-16SrDNA-OTU-Clustering/mock_run3/input \
   --work-dir ~/host/project/imicrobe/apps/imicrobe-16SrDNA-OTU-Clustering/mock_run3/container_output \
-  --core-count 1 \
+  --core-count 2 \
   --cutadapt-min-length 100 \
   --pear-min-overlap 200\
   --pear-max-assembly-length 270 \
   --pear-min-assembly-length 220 \
   --vsearch-filter-maxee 1 \
-  --vsearch-filter-trunclen 245
+  --vsearch-filter-trunclen 245 \
+  --vsearch-derep-minuniquesize 3
