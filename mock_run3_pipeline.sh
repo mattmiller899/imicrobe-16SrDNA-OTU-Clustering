23a5707e3528e77e26761b3c589100d0ee3498f7
Joshua Lynch <jklynch@email.arizona.edu>
2017-06-29 14:20:27 +0100

    add argument for step 06

--- mock_run3_pipeline.sh
+++ mock_run3_pipeline.sh
@@ -6,10 +6,12 @@ mkdir -p mock_run3/pipeline_output
 time pipeline \
   --input-dir ~/host/project/imicrobe/apps/imicrobe-16SrDNA-OTU-Clustering/mock_run3/input \
   --work-dir ~/host/project/imicrobe/apps/imicrobe-16SrDNA-OTU-Clustering/mock_run3/pipeline_output \
-  --core-count 1 \
+  --core-count 2 \
   --cutadapt-min-length 100 \
   --pear-min-overlap 200\
   --pear-max-assembly-length 270 \
   --pear-min-assembly-length 220 \
   --vsearch-filter-maxee 1 \
-  --vsearch-filter-trunclen 245
+  --vsearch-filter-trunclen 245 \
+  --vsearch-derep-minuniquesize 3
+
