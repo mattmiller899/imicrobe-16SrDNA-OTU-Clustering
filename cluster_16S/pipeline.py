fc117d6ee2e87f0024f5dc99ab0f7db6195c95ec
mattmiller899 <mattmiller899@email.arizona.edu>
2017-11-28 10:15:49 -0700

    Changed Step 08 to use vsearch
    I could not get usearch --uchime-ref to work with a full-sized SILVA database, even when I used a <1mb fasta file.

--- cluster_16S/pipeline.py
+++ cluster_16S/pipeline.py
@@ -511,31 +511,15 @@ class Pipeline:
                 log.info('starting chimera detection on file "%s"', input_fp)
 
                 run_cmd([
-                        self.usearch_executable_fp,
-                        '-uchime2_ref', input_fp,
-                        '-db', self.uchime_ref_db_fp,
-                        '-uchimeout', uchimeout_fp,
-                        '-mode', 'balanced',
-                        '-strand', 'plus',
-                        '-notmatched', notmatched_fp,
-                        '-threads', str(self.core_count)
-                    ],
-                    log_file=os.path.join(output_dir, 'log')
-                )
-                '''
-                run_cmd([
                         self.vsearch_executable_fp,
                         '-uchime_ref', input_fp,
                         '-db', self.uchime_ref_db_fp,
                         '-uchimeout', uchimeout_fp,
-                        '-mode', 'balanced',
-                        '-strand', 'plus',
-                        '-notmatched', notmatched_fp,
+                        '-nonchimeras', notmatched_fp,
                         '-threads', str(self.core_count)
                     ],
                     log_file = os.path.join(output_dir, 'log')
                 )
-                '''
 
         self.complete_step(log, output_dir)
         return output_dir
